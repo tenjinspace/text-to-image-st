aaba21be2d1ee6e1c8e478e4cf570c6b3d8b2cb2
tenjin <103417420+tenjinspace@users.noreply.github.com>
2022-11-01 16:20:07 +0100

    dev

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+import streamlit as st
+import pandas as pd
 
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+today = date.today()
+
+st.title('Image generator with Stable Diffusion')
+st.header('text-to-image AI model')
+st.subheader('Today is: {}'.format(today))
+
+st.dataframe(df)
