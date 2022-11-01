d74ca744a7ebd4c6bc80fc6cabc8037e4e59284c
tenjin <103417420+tenjinspace@users.noreply.github.com>
2022-11-01 17:42:01 +0100

    dev

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 import streamlit as st
-import pandas as pd
 from datetime import date
 
 today = date.today()
 
-st.title('Image generator with Stable Diffusion')
-st.header('text-to-image AI model')
-st.subheader('Today is: {}'.format(today))
+def main():
+    st.title("Image generator with Stable Diffusion")
+    st.markdown("text-to-image AI model")
 
-objects = st.text_input('Object(s) on the scene', 'a bear and a rooster')
-background = st.text_input('Background of the scene', 'in a cyberpunk destroyed city')
-query = objects + ' ' + background
-st.write('Your input: ', query)
+    today = date.today()
+    st.header('Today is: {}'.format(today))
 
-image = ''
+    objects = st.text_input('Object(s) on the scene', 'a bear and a rooster')
+    background = st.text_input('Background of the scene', 'in a cyberpunk destroyed city')
+    query = objects + ' ' + background
 
-st.image(image, caption=query)
+    st.write('Your input: ', query)
+
+if __name__ == "__main__":
+    main()
